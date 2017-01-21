cf26fe2f97494899df6879fe0bb53557cf444c64
Frank Martin <frank.martin@digital-results.com>
2017-01-21 10:41:20 +0000

    Add merge sort

--- sorting.go
+++ sorting.go
@@ -1,4 +1,4 @@
-package sorting
+package main
 
 // Bubble - Loop through the array check each adjacent number if the next one is bigger than the current then switch them
 func Bubble(arr []int) []int {
@@ -33,4 +33,37 @@ func Insertion(arr []int) []int {
 	}
 
 	return arr
+}
+
+func merge(l, r []int) []int {
+	result := make([]int, 0, len(l)+len(r))
+
+	for len(l) > 0 || len(r) > 0 {
+		if len(l) == 0 {
+			return append(result, r...)
+		}
+		if len(r) == 0 {
+			return append(result, l...)
+		}
+		if l[0] <= r[0] {
+			result = append(result, l[0])
+			l = l[1:]
+		} else {
+			result = append(result, r[0])
+			r = r[1:]
+		}
+	}
+
+	return result
+}
+
+// Merge - Recursively split the array until all is left are many arrays of 1 item, then stitch them back together in order
+func Merge(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	mid := len(arr) / 2
+	l := Merge(arr[:mid])
+	r := Merge(arr[mid:])
+	return merge(l, r)
 }
